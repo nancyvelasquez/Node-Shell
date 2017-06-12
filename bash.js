6ced490696d5595d729ec0831313afc723fb42c6
Olivia Oddo <olivia.oddo@gmail.com>
2017-06-12 12:53:51 -0400

    node shell in progress

--- bash.js
+++ bash.js
@@ -1,5 +1,10 @@
 var commands = require('./commands')
 
+var done = function(output){
+    process.stdout.write('' + output);
+    process.stdout.write('\nprompt > ');
+};
+
 
 // Output a prompt
 process.stdout.write('prompt > ');
@@ -7,7 +12,10 @@ process.stdout.write('prompt > ');
 // The stdin 'data' event fires after a user types in a line
 process.stdin.on('data', function (data) {
   var cmd = data.toString().trim(); // remove the newline
-
-  commands[cmd]();
+  if(cmd.startsWith('echo'))  commands['echo'](cmd.slice(5), done);
+  else if(cmd.startsWith('cat'))  commands['cat'](cmd.split(' ').slice(1), done);
+  else if(cmd.startsWith('head'))  commands['head'](cmd.slice(5), done);
+  else if(cmd.startsWith('tail'))  commands['tail'](cmd.slice(5), done);
+  else commands[cmd](null, done);
 
 });
--- commands.js
+++ commands.js
@@ -11,17 +11,45 @@ var fs = require('fs');
 //   }
 // };
 
-exports.pwd = function() {
-    process.stdout.write('' + process.env.PWD);
-    process.stdout.write('\nprompt > ');
+exports.pwd = function(arg, done) {
+    done(process.env.PWD);
 };
 
-exports.date = function() {
-    process.stdout.write('' + new Date());
-    process.stdout.write('\nprompt > ');
+exports.date = function(arg, done) {
+    done(new Date());
 };
 
-exports.ls = function(){
-  process.stdout.write('' + fs.readdirSync('.').join('\n'));
-  process.stdout.write('\nprompt > ');
+exports.ls = function(arg, done){
+  // print(fs.readdirSync('.').join('\n'));
+   fs.readdir('.', (err, files) => {if(err) done(err); else (done(files)) })
 };
+
+exports.echo = function(arg, done){
+    if(arg === '$PATH') done(process.env.PATH);
+    else done(arg);
+};
+
+exports.cat = function(arg, done){
+    var str = '';
+    arg.forEach(function(element){
+        fs.readFile(process.env.PWD +'/' + element, 'utf-8', (err, data) => {if(err) done(err);
+        else done(data)});
+    });
+
+    // done(data);
+
+};
+
+exports.head = function(arg, done){
+    fs.readFile(process.env.PWD +'/' + arg, 'utf-8', (err, data) => {if(err) done(err);
+        else done(data.split('\n').slice(0, 5).join('\n'));});
+
+};
+
+exports.tail = function(arg, done){
+    fs.readFile(process.env.PWD +'/' + arg, 'utf-8', (err, data) => {if(err) done(err);
+    else done(data.split('\n').slice(data.split('\n').length - 5).join('\n'));});
+
+};
+
+
